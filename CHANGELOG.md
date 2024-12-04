fdb4bf928436717d30673031b803269be2fabda3
Giulio Calzolari <9049490+giuliocalzolari@users.noreply.github.com>
2024-12-04 21:15:17 +0100

    update install script (#6)
    Co-authored-by: Giulio <giuliocalzolari@users.noreply.github.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,10 @@
 All notable changes are documented in this file
 
 
+## 0.0.3
+
+- adjust install script
+
 ## 0.0.2
 
 - adding CodeQL action
---
+++ scripts/install-binary.sh
@@ -0,0 +1,124 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+
+PROJECT_NAME="helm-datarobot-plugin"
+PROJECT_GH="datarobot-oss/$PROJECT_NAME"
+
+HELM_PLUGIN_NAME="helm-datarobot"
+HELM_PLUGINS=$(helm env | grep 'HELM_PLUGINS' | cut -d '=' -f2 | tr -d '"')
+HELM_HOME=$(helm env | grep 'HELM_DATA_HOME' | cut -d '=' -f2 | tr -d '"')
+HELM_PLUGIN_DIR="$HELM_HOME/plugins/$HELM_PLUGIN_NAME"
+
+if [ -n "${SKIP_BIN_INSTALL:-}" ]; then
+    echo "Development mode: not downloading versioned release."
+    exit 0
+fi
+
+
+validate_checksum() {
+    if ! grep -q ${1} ${2}; then
+        echo "Invalid checksum" > /dev/stderr
+        exit 1
+    fi
+    echo "Checksum is valid."
+}
+
+initArch() {
+  ARCH=$(uname -m)
+  case $ARCH in
+    x86_64|amd64) ARCH="amd64" ;;
+    aarch64|arm64) ARCH="arm64" ;;
+    *)
+      echo "Arch '$(uname -m)' not supported!" >&2
+      exit 1
+      ;;
+  esac
+
+}
+
+initOS() {
+  OS=$(uname -s)
+  case "$(uname)" in
+    Darwin) OS="darwin" ;;
+    Linux) OS="linux" ;;
+    CYGWIN*|MINGW*|MSYS_NT*) OS="windows" ;;
+    *)
+      echo "OS '$(uname)' not supported!" >&2
+      exit 1
+      ;;
+  esac
+}
+
+
+downloadRelease() {
+  PLUGIN_VERSION=$(grep -e  '^version' plugin.yaml | sed -e 's/^version: //')
+  echo "Downloading and installing ${PROJECT_NAME} ${PLUGIN_VERSION} ..."
+  BIN_URL="https://github.com/${PROJECT_GH}/releases/download/${PLUGIN_VERSION}/${PROJECT_NAME}-${OS}-${ARCH}"
+  CHECKSUM_URL="https://github.com/${PROJECT_GH}/releases/download/${PLUGIN_VERSION}/${PROJECT_NAME}_${PLUGIN_VERSION}_checksums.txt"
+
+  PLUGIN_TMP_FILE="/tmp/${PROJECT_NAME}-${PLUGIN_VERSION}-${OS}-${ARCH}"
+  CHECKSUM_TMP_FILE="/tmp/${PROJECT_NAME}_${PLUGIN_VERSION}_checksums.txt"
+
+  echo "Downloading $BIN_URL"
+  if type "curl" > /dev/null; then
+    curl -L "$BIN_URL" -o "$PLUGIN_TMP_FILE"
+    curl -L "$CHECKSUM_URL" -o "$CHECKSUM_TMP_FILE"
+  elif type "wget" > /dev/null; then
+    wget -q -O "$PLUGIN_TMP_FILE" "$BIN_URL"
+    wget -q -O "$CHECKSUM_TMP_FILE" "$CHECKSUM_URL"
+  fi
+
+  if command -v sha256sum >/dev/null 2>&1; then
+    checksum=$(sha256sum ${PLUGIN_TMP_FILE} | awk '{ print $1 }')
+    validate_checksum ${checksum} ${CHECKSUM_TMP_FILE}
+  elif command -v openssl >/dev/null 2>&1; then
+    checksum=$(openssl dgst -sha256 ${PLUGIN_TMP_FILE} | awk '{ print $2 }')
+    validate_checksum ${checksum} ${CHECKSUM_TMP_FILE}
+  else
+    echo "WARNING: no tool found to verify checksum" > /dev/stderr
+  fi
+}
+
+# downloadFile downloads the latest binary package and also the checksum
+# for that binary.
+installBin() {
+  echo "Preparing to install into ${HELM_PLUGIN_DIR}"
+  mkdir -p "$HELM_PLUGIN_DIR/bin"
+  mv $PLUGIN_TMP_FILE "$HELM_PLUGIN_DIR/bin/$PROJECT_NAME"
+  chmod +x "$HELM_PLUGIN_DIR/bin/$PROJECT_NAME"
+}
+
+# testVersion tests the installed client to make sure it is working.
+testVersion() {
+  set +e
+  echo "$PROJECT_NAME installed into $HELM_PLUGIN_DIR/bin/$PROJECT_NAME"
+  "${HELM_PLUGIN_DIR}/bin/$PROJECT_NAME" version
+  set -e
+}
+
+
+
+# fail_trap is executed if an error occurs.
+fail_trap() {
+  result=$?
+  if [ "$result" != "0" ]; then
+    echo "Failed to install $PROJECT_NAME"
+    echo "\tFor support, go to https://github.com/kubernetes/helm."
+  fi
+  exit $result
+}
+
+
+
+# Execution
+
+#Stop execution on any error
+trap "fail_trap" EXIT
+set -e
+initArch
+initOS
+downloadRelease
+installBin
+testVersion
