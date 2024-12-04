0a4ee4e3760d9a522ea6a56854bcc893dd032d65
Giulio Calzolari <9049490+giuliocalzolari@users.noreply.github.com>
2024-12-04 22:20:50 +0100

    Update validate.sh

--- .github/scripts/validate.sh
+++ .github/scripts/validate.sh
@@ -7,12 +7,6 @@ git config --global url."https://oauth2:$GITHUB_TOKEN@github.com".insteadOf http
 git fetch --tags
 
 PLUGIN_VERSION=$(grep -e  '^version' plugin.yaml | sed -e 's/^version: //')
-if [[ $PLUGIN_VERSION != $CHANGELOG_VERSION ]]; then
-    echo "found [$PLUGIN_VERSION] in plugin.yaml and mismatch with Change Log version [$CHANGELOG_VERSION]"
-    exit 1
-fi
-
-
 if git show-ref --tags | grep -q "refs/tags/$PLUGIN_VERSION"; then
     echo "The tag $PLUGIN_VERSION already exists, exiting."
     exit 1
