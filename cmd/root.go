4e6674921f91c6924673108251b18128633e8e7a
Giulio Calzolari <9049490+giuliocalzolari@users.noreply.github.com>
2024-12-04 22:33:53 +0100

    set version using cobra and goreleaser (#19)
    Co-authored-by: Giulio <giuliocalzolari@users.noreply.github.com>

--- cmd/root.go
+++ cmd/root.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"regexp"
 
 	"github.com/spf13/cobra"
 )
@@ -16,6 +17,13 @@ var rootCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("Use --help for more information.")
 	},
+}
+
+func SetVersionInfo(version, commit string) {
+	re := regexp.MustCompile(`\d+\.\d+\.\d+`)
+	// Find the first match of the pattern in the version string
+	semver := re.FindString(version)
+	rootCmd.Version = fmt.Sprintf("%s (Git SHA %s)", semver, commit)
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
