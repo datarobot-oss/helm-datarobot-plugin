25a1740f8c6f6936f152fb4b3993608b947eff7d
Giulio <giuliocalzolari@users.noreply.github.com>
2024-12-10 09:31:52 +0100

    ddd

--- cmd/generate_test.go
+++ cmd/generate_test.go
@@ -18,4 +18,17 @@ func TestCommandGenerate(t *testing.T) {
       image: docker.io/alpine/curl:8.9.1`
 		assert.Equal(t, expectedOutput, output)
 	})
+	t.Run("test-chart1", func(t *testing.T) {
+		output, err := executeCommand(rootCmd, "generate ../testdata/test-chart1")
+		assert.NoError(t, err)
+		expectedOutput := `annotations:
+  datarobot.com/images: |
+    - name: test-image3_300
+      image: docker.io/datarobotdev/test-image3:3.0.0
+    - name: test-image2_200
+      image: docker.io/datarobotdev/test-image2:2.0.0
+    - name: test-image1_100
+      image: docker.io/datarobotdev/test-image1:1.0.0`
+		assert.Equal(t, expectedOutput, output)
+	})
 }
--- cmd/validate_test.go
+++ cmd/validate_test.go
@@ -7,14 +7,12 @@ import (
 )
 
 func TestCommandValidate(t *testing.T) {
-
-	// TODO for some reason does not work when is called with executeCommand but it work when in invoked as cli
-	// t.Run("test-chart1", func(t *testing.T) {
-	// 	output, err := executeCommand(rootCmd, "validate", "../testdata/test-chart1", "-a", "'datarobot.com/images'", "--debug")
-	// 	assert.NoError(t, err)
-	// 	expectedOutput := `Image Doc Valid`
-	// 	assert.Equal(t, expectedOutput, output)
-	// })
+	t.Run("test-chart1", func(t *testing.T) {
+		output, err := executeCommand(rootCmd, "validate ../testdata/test-chart1 -a \"datarobot.com/images\"")
+		assert.NoError(t, err)
+		expectedOutput := `Image Doc Valid`
+		assert.Equal(t, expectedOutput, output)
+	})
 	t.Run("test-chart5", func(t *testing.T) {
 		output, err := executeCommand(rootCmd, "validate ../testdata/test-chart5")
 		assert.NoError(t, err)
@@ -24,7 +22,7 @@ func TestCommandValidate(t *testing.T) {
 	t.Run("test-chart5/error", func(t *testing.T) {
 		output, err := executeCommand(rootCmd, "validate ../testdata/test-chart5 -a \"custom/images-wrong\"")
 		assert.Error(t, err)
-		expectedOutput := `Error: Image not defined in as imageDoc: busybox:1.36.1`
+		expectedOutput := `Error: Image not defined in as imageDoc: docker.io/alpine/curl:8.9.1`
 		assert.Equal(t, expectedOutput, output)
 	})
 	t.Run("test-chart5/empty", func(t *testing.T) {
