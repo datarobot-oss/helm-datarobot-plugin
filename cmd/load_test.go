9f2ef5f982c0d4966b09ce8efd966b992fc8c0d1
Giulio <giuliocalzolari@users.noreply.github.com>
2025-04-07 13:51:40 +0200

    xxx

--- cmd/load_test.go
+++ cmd/load_test.go
@@ -46,8 +46,7 @@ Tarball created successfully: image-load.tar.zst`
 		os.Setenv("REGISTRY_HOST", "localhost:5000")
 		output, err := executeCommand(rootCmd, "load "+LOAD_TEST_ARCHIVE+" --dry-run --skip-image alpine/curl:8.9.1 ")
 		assert.NoError(t, err)
-		expectedLoadOutput := `[Dry-Run] Pushing image: localhost:5000/alpine/curl:8.9.1
-[Dry-Run] Pushing image: localhost:5000/busybox:1.36.1`
+		expectedLoadOutput := `Skipping image: alpine/curl:8.9.1`
 		assert.Equal(t, expectedLoadOutput, output)
 	})
 	t.Run("local-registry-insecure", func(t *testing.T) {
